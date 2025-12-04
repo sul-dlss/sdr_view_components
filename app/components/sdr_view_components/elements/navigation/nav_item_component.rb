282a62f6402451a6beaddd2af24e09a96d797dc0
Aaron Collier <aaron.collier@stanford.edu>
2025-12-04 09:38:34 -0800

    Update nav item to allow data

--- app/components/sdr_view_components/elements/navigation/nav_item_component.rb
+++ app/components/sdr_view_components/elements/navigation/nav_item_component.rb
@@ -5,13 +5,14 @@ module SdrViewComponents
     module Navigation
       # Component for rendering an alert.
       class NavItemComponent < BaseComponent
-        def initialize(text:, path:)
+        def initialize(text:, path:, data: {})
           @text = text
           @path = path
+          @data = data
           super()
         end
 
-        attr_reader :text, :path
+        attr_reader :text, :path, :data
       end
     end
   end
