86a717ea453b11f38514d6b77514bdcd5ce7e186
Justin Littman <justinlittman@stanford.edu>
2026-05-11 15:16:42 -0400

    More work

---
+++ spec/components/previews/sdr_view_components/forms/basic_checkbox_component_preview.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module SdrViewComponents
+  module Forms
+    class BasicCheckboxComponentPreview < ViewComponent::Preview
+      def default; end
+
+      # @!group Checkbox State
+      def checked; end
+
+      def disabled; end
+      # @!endgroup
+    end
+  end
+end
---
+++ spec/components/previews/sdr_view_components/forms/checkbox_component_preview.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module SdrViewComponents
+  module Forms
+    class CheckboxComponentPreview < ViewComponent::Preview
+      def default; end
+
+      # @!group Checkbox State
+      def checked; end
+
+      def disabled; end
+      # @!endgroup
+
+      def with_help_text; end
+
+      def with_validation_error; end
+
+      class CheckboxPreviewModel
+        include ActiveModel::Model
+        include ActiveModel::Attributes
+
+        attribute :test_boolean_field, :boolean, default: false
+
+        validates :test_boolean_field, acceptance: true
+      end
+    end
+  end
+end
